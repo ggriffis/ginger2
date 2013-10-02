efa3856f592cd4aef51b66e001db50f4a2eb77fa
Ginger Griffis <gingergriffis@hotmail.com>
2013-10-02 12:12:39 -0700

    Updated home page to use web dev attributes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
   def index
+    @web_developer = WebDeveloper.all.first
+    if @web_developer && @web_developer.main_image
+      @web_developer_image_url = @web_developer.main_image.image.to_s
+    else
+      @web_developer_image_url = "web_dev.jpg"
+    end
   end
 end
--- app/models/web_developer.rb
+++ app/models/web_developer.rb
@@ -11,4 +11,11 @@ class WebDeveloper < ActiveRecord::Base
 
   accepts_nested_attributes_for :web_developer_links, :web_developer_images, :web_developer_endorsements, :web_developer_projects, :web_developer_jobs,
                                 :web_developer_trainings, allow_destroy: true
+
+  def main_image
+    proc = Proc.new {self.web_developer_images.first}
+    self.web_developer_images.detect(proc) do |e|
+      e.main_image?
+    end
+  end
 end
